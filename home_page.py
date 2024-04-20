24ad3ee8f9df33c74944a1a7104abd8f20ad47ec
MattDuyv <mduyv001@fiu.edu>
2024-04-20 14:27:04 -0400

    Connection to database established

--- home_page.py
+++ home_page.py
@@ -1,10 +1,51 @@
 import streamlit as st
+import mysql.connector
+from passlib.hash import pbkdf2_sha256
+
+# Initialize SQLite connection
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="scrape",
+    password="password",
+    database="fooddb"
+)
+
+if mydb.is_connected():
+    print('Yes')
+
+def verifyLogin(conn, username, password):
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM user WHERE username=%s", (username,))
+
+    row = cursor.fetchone()
+
+    print(row[3])
+
+    if row:
+        hashedPassword = row[3]
+        if pbkdf2_sha256.verify(password, hashedPassword):
+            return True
+    return False
+
+def showSignInPopup():
+    # Create input fields for username and password
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+
+    # Create a button to submit the sign-in credentials
+    if st.button('Sign In'):
+        if verifyLogin(mydb, username, password):
+            st.success('Login successful!')
+            st.session_state.show_login_popup = False  # Hide the popup
+        else:
+            st.error('Invalid username or password')
 
 if __name__ == "__main__":
     st.title("Calorie Scraper")
-    st.markdown("---")
 
     if st.button("Login"):
-        st.switch_page("pages/login_page.py")
-    if st.button("Logout"):
-        st.switch_page("pages/sign_up_page.py")
+        st.session_state.show_login_popup = True
+
+    if st.session_state.get('show_login_popup'):
+        showSignInPopup()
