f0b2ad9579e11053fc0d3653190efd875c8bcadf
MattDuyv <mduyv001@fiu.edu>
2024-04-21 13:44:31 -0400

    Persisting user session updated, includes sign in and sign up now

--- home_page.py
+++ home_page.py
@@ -40,7 +40,7 @@ def showSignInPopup():
     if st.button('Sign In'):
         if not(username) or not(password):
             st.error('Missing username or password')
-        elif verifyLogin(mydb, username, password):
+        elif verifyLogin(conn, username, password):
             st.success('Login successful!')
             st.session_state.show_login_popup = False
         else:
@@ -63,8 +63,8 @@ def showSignUpPopup():
     height2 = col2.number_input("Height (in)", min_value=1, max_value=11, step=1, value=8)
 
     if st.button('Create Account'):
-        uExist = check_username(mydb, newUsername)
-        eExist = checkEmail(mydb, newEmail)
+        uExist = check_username(conn, newUsername)
+        eExist = checkEmail(conn, newEmail)
         print(uExist, eExist)
         if not (newUsername) or not (newPassword) or not (newEmail) or not(verifyPass) or not(gender) or not(age) or not(weight) or not(height1) or not (height2):
             st.error('Please fill in all fields')
@@ -72,7 +72,7 @@ def showSignUpPopup():
             st.error('Passwords do not match')
         elif not (uExist) and not (eExist):
             height = (height1*12) + height2
-            add_user(mydb, newUsername, newPassword, newEmail, height, weight, age, gender)
+            add_user(conn, newUsername, newPassword, newEmail, height, weight, age, gender)
             st.success('Account created successfully!')
         elif uExist:
             st.warning('Username already exists!')
--- session_state.py
+++ session_state.py
@@ -4,3 +4,4 @@ def init():
     if 'userID' not in st.session_state:
         st.session_state.userID = None
         st.session_state.username = None
+        st.session_state.deleteConfirmation = False
