7e1fccb18043e6411c7d0ae62615c54df804ec29
Puchor <ruinrake@gmail.com>
2024-04-21 18:28:38 -0400

    organized goals

--- pages/user_main_page.py
+++ pages/user_main_page.py
@@ -32,6 +32,7 @@ if st.session_state.conn:
         user_food_goals = db_utils.get_user_food_goals(st.session_state.conn, st.session_state.userID)
         if user_food_goals:
             for goal in user_food_goals:
+                st.write(f"- **Goal ID:** {goal['goal_id']}")
                 st.write(f"- **Goal Type:** {goal['goal_type']}")
                 st.write(f"  - Target Weight: {goal['target_weight']}")
                 st.write(f"  - Target Calories per Day: {goal['target_calories_per_day']}")
@@ -39,6 +40,10 @@ if st.session_state.conn:
                 st.write(f"  - Target Carbs per Day: {goal['target_carbs_per_day']}")
                 st.write(f"  - Target Fat per Day: {goal['target_fat_per_day']}")
                 st.write(f"  - End Date: {goal['end_date']}")
+                if st.button("Delete Goal"):
+                    db_utils.delete_user_food_goal(st.session_state.conn, st.session_state.userID, goal['goal_id'])
+                    st.success("Goal deleted successfully!")
+                st.markdown("---")
         else:
             st.write("No food goals found for the user.")
 
@@ -54,6 +59,7 @@ if st.session_state.conn:
             st.switch_page("pages/search_page.py")
         if options.button("Add New Food Goal", key="button2") and not st.session_state.open:
             st.session_state.open = True
+
         if st.session_state.open:
             goal_info = {}  # Example goal information, you can change this
             goal_info['goal_type'] = st.selectbox("Goal Type", ["Weight Loss", "Weight Gain", "Maintenance"])
@@ -64,8 +70,16 @@ if st.session_state.conn:
             goal_info['target_fat_per_day'] = st.number_input("Target Fat per Day", value=0, step=1)
             goal_info['end_date'] = st.date_input("End Date")
 
-    else:
-        st.error("Failed to retrieve user information. Please try again later.")
+            # Insert the new food goal into the database
+            try:
+                db_utils.log_new_food_goal(st.session_state.conn, st.session_state.userID, goal_info)
+                st.success("New food goal added successfully!")
+            except Error as e:
+                st.error("Failed to add new food goal. Please try again later.")
+
+        else:
+            st.error("Failed to retrieve user information. Please try again later.")
+
 
     st.write("\n")
     if st.button("Sign Out"):
@@ -73,4 +87,5 @@ if st.session_state.conn:
         st.switch_page("home_page.py")
 else:
     st.error("Failed to connect to the database. Please try again later.")
+
 
